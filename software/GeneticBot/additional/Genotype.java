6772b148b3035b751d1e31084bf142ec63d6b15b
zak <zak@folkvangr.localdomain>
2019-08-28 19:34:06 +0100

    Add new files

---
+++ software/GeneticBot/additional/Genotype.java
@@ -0,0 +1,360 @@
+/**
+ * @author:  Zak Edwards
+ * @version: 1.0 25/03/16
+ */
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Genotype {
+
+    /* ------------------- Determine the character and behaviour of the phenotype ------------------- */
+    final static String
+        Cnst[] =
+                    {
+                     "0.001",
+                     "Math.PI",
+                     "Math.random()",
+                     "Math.floor(Math.random()*10)",
+                     "Double.toString(rand.nextDouble())"    // Ephemeral random constants
+                    },
+        Univ[] =
+                    {
+                     "getEnergy()",  "getVelocity()",
+                     "getHeight()",  "getWidth()",
+                     "getHeading()", "getBearing()",
+                     "getX()",       "getY()"
+                    },
+        Evnt[] =
+                    {
+                     "e.getEnergy",    "e.getVelocity()",
+                     "e.getHeading()", "e.getBearing()",
+                     "e.getDistance()"
+                    },
+        Term[][] =
+                    {
+                     Cnst, Univ, Evnt
+                    },
+        Func[][] =
+                    {
+                     {"Math.sin(", ")"}, {"Math.asin(", ")"},
+                     {"Math.cos(", ")"}, {"Math.acos(", ")"},
+                     {"Math.toDegrees(", ")"}, {"Math.toRadians(", ")"},
+                     {"Math.min(", ",",  ")"}, {"Math.max(", ",",  ")"}, {"Math.abs(", ")"},
+                     {"", " * -1"}    // Sign inversion
+                    },
+        Bnry[][] =
+                    {
+                     {"", "+", ""},
+                     {"", "-", ""},
+                     {"", "/", ""},
+                     {"", "*", ""}
+                    },
+        Cond1[][] =
+                    {
+                     /* If-then-else conditionals */
+                     {"", " > 0 ? ", " : ", ""}
+                    },
+        Cond2[][] =
+                    {
+                     {"", " > ",  " ? ", " : ", ""},
+                     {"", " == ", " ? ", " : ", ""},
+                     /* While statement */
+                     {"while", "", ""}
+                    },
+        Expr[][][] =
+                    {
+                     Term, Func, Bnry, Cond1, Cond2
+                    };
+
+    /* Probabilities for terminal nodes characterised by the BNF grammar */
+    final static double
+        pTermCnst = 0.15,    // Terminals of BNF-type <cnst>
+        pTermUniv = 0.35,    // Terminals of BNF-type <univ>
+        pTermEvnt = 0.40,    // Terminals of BNF-type <evnt>
+        pTermERC  = 0.10,    // Ephemeral Random Constants of BNF-type <cnst>
+        pTerm[]   = {
+                     pTermCnst,
+                     pTermUniv,
+                     pTermEvnt,
+                    },
+
+        pBnry   = 0.60,
+        pFunc   = 0.20,
+        pCond1  = 0.05,      // p(Cond[1][])
+        pCond2  = 0.15,      // p(Cond[2-4][])
+        pExpr[] = {
+                   pBnry,
+                   pFunc,
+                   pCond1,
+                   pCond2
+                  };
+
+    /* Determine the constraints on the depths of expression trees */
+    final static int
+        minDepth = 3,
+        maxDepth = 10;
+
+    /* Probabilities for operations on expression trees */
+    final static double
+        /* Crossover */
+        pCrossTerm = 0.10,
+        pCrossRoot = 0.30,
+        pCrossFunc = 0.60,
+        /* Mutation */
+        pMutateTerm = 0.10,
+        pMutateRoot = 0.30,
+        pMutateFunc = 0.60;
+
+    int depth = 0, arity = -1;    // We define arity of a vertex to be the number of children for which the vertex is a parent
+    boolean isTerm = true;        // return true if the arity of the concerned vertex is zero
+
+    Genotype children[];          // An array to populated with vertices of non-zero depth
+    String expression[];          // An array to be populated with legal expressions
+
+    static Random rand = new Random(System.currentTimeMillis());
+
+    public Genotype(int depth) {
+        this.depth = depth;
+    }
+
+    public Genotype(int depth, int arity, boolean isTerm) {
+        this.depth  = depth;
+        this.arity  = arity;
+        this.isTerm = isTerm;
+    }
+
+    /**
+     * Conduct depth-first tree traversal (composition) on the expression tree.
+     */
+    public String compose() {
+        String wff = expression[0];
+        if (this.arity == -1) {
+            System.err.println("Err> nonpositive arity encountered.");
+        }
+        for (int i = 0; i < this.arity; i++) {
+            //System.out.println(arity);
+            try {
+                wff += (children[i].compose() + expression[i + 1]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new RuntimeException("Tried to access index " + i + " but array has only " + expression.length + " elements", e);
+            }
+        }
+        wff = "(" + wff + ")";   // Ensure syntactic ambiguity is avoided
+        System.out.println("  " + wff);
+        return wff;
+    }
+
+    public int countNodes() {
+        int count = 1;
+        for (int i = 0; i < this.arity; i++) {
+            count += this.children[i].countNodes();
+        }
+        return count;
+    }
+
+    /**
+     * A function, recursively-called, for the expansion of the expression tree.
+     */
+    public void expand(int depth, int expr) {
+        setArity(depth);
+        assignExpr(depth, expr);
+    }
+
+    private void assignExpr(int depth, int expr) {
+        if (this.arity == 0) {
+            assignTerm();
+        } else {
+            //System.out.print(arity + " ");
+            this.expression = Expr[arity][rand.nextInt(Expr[arity].length)];
+            for (int i = 0; i < arity; i++) {
+                children[i] = new Genotype(depth + 1);
+                children[i].expand((depth + 1), expr);
+            }
+        }
+    }
+
+    private void assignTerm() {
+        expression = new String[1];
+        double rnd = rand.nextDouble();
+        this.children = null;
+        for (int i = 0; i < Term.length; i++) {
+           if ((rnd -= pTerm[i]) <= 0) {
+               expression[0] = Expr[0][i][rand.nextInt(Expr[0][i].length)];
+               break;
+           }
+        }
+        if (rnd > 0) {
+            //if (pTermERC < rand.nextDouble())
+            //    return;
+            expression[0] = Double.toString(rand.nextDouble());
+        }
+    }
+
+    public int getMaxNode() {
+        int maxNode = this.depth;
+        for (int i = 0; i < this.arity; i++) {
+            maxNode = Math.max(children[i].getMaxNode(), maxNode);
+        }
+        return maxNode;
+    }
+
+    public int getMinTerm() {
+        if (isTerm) {
+            return this.depth;
+        } else {
+            int minTerm = (maxDepth + 1);
+            for (Genotype expr : children) {
+                minTerm = Math.min(minTerm, expr.getMinTerm());
+            }
+            return minTerm;
+        }
+    }
+
+    /**
+     * Assign arity to vertices via selection subroutine.
+     */
+    public void setArity(int depth) {
+        /* (depth > minDepth) -> isTerm; (depth == maxDepth) -> (isTerm v !isTerm) */
+        if (((depth > minDepth) && (rand.nextDouble() < 0.35)) || depth == maxDepth) {
+            this.arity = 0;
+            isTerm = true;
+        } else {
+            isTerm = false;
+            double rnd = rand.nextDouble();
+            /* Selection subroutine */
+            for (int i = 0; i < pExpr.length; i++) {
+                if ((rnd -= pExpr[i]) <= 0) {
+                    this.arity = (i + 1);
+                    break;
+                }
+            }
+            if (rnd > 0) {
+                this.arity = pExpr.length;
+            }
+            children = new Genotype[this.arity];
+        }
+    }
+
+    public void setDepths(int depth) {
+        this.depth = depth;
+        for (int i = 0; i < this.arity; i++) {
+            this.children[i].setDepths(depth + 1);
+        }
+    }
+
+    public void insert(Genotype newNode) {
+        int maxNode     = this.getMaxNode();
+        int minTerm     = newNode.getMinTerm();
+        int minSubTerm  = (minTerm - newNode.depth);
+        int maxSubDepth = (newNode.getMaxNode() - newNode.depth);
+
+        int ceiling = (maxDepth - maxSubDepth);
+        int floor   = Math.max(1, (minDepth - maxSubDepth));
+
+        int targetDepth = (rand.nextInt((ceiling - floor) + 1) + floor);
+
+        if ((t
+            argetDepth + minSubTerm) < minDepth) {
+            targetDepth = minDepth - minSubTerm;
+        }
+        if (maxNode < targetDepth) {
+            this.insertAt(newNode, maxNode);
+        } else {
+            this.insertAt(newNode, targetDepth);
+        }
+    }
+
+    public void insertAt(Genotype newNode, int target) {
+        if (this.depth == target) {
+            this.replace(newNode);
+        } else {
+            ArrayList<Integer> genome = new ArrayList<Integer>();
+            for (int i = 0; i < this.arity; i++) {
+                if (children[i].getMaxNode() >= target) {
+                    genome.add(i);
+                }
+            }
+            int targetBranch = genome.get(rand.nextInt(genome.size()));
+            children[targetBranch].insertAt(newNode, target);
+        }
+    }
+
+    public void replace(Genotype newNode) {
+        this.arity      = newNode.arity;
+        this.isTerm     = newNode.isTerm;
+        this.expression = new String[newNode.expression.length];
+        for (int i = 0; i < this.expression.length; i++)
+            this.expression[i] = newNode.expression[i];
+        if (newNode.isTerm) {
+            this.children = null;
+        } else {
+            this.children = new Genotype[arity];
+            for (int i = 0; i < newNode.arity; i++) {
+                this.children[i] = new Genotype(depth + 1);
+                this.children[i].replace(newNode.children[i]);
+            }
+        }
+    }
+
+    public void mutateTerm() {
+        if (!this.isTerm) {
+            this.children[rand.nextInt(this.arity)].mutateTerm();
+        } else {
+            this.assignTerm();
+        }
+    }
+
+    public void mutateFunc() {
+        if (this.depth == 0) {
+            this.children[rand.nextInt(this.arity)].mutateFunc();
+        } else if (this.depth == (maxDepth - 1) || rand.nextDouble() < 0.3) {
+            Genotype newSubTree = new Genotype(this.depth);
+            newSubTree.expand(this.depth, 0);
+            this.replace(newSubTree);
+        }
+    }
+
+    public Genotype clone() {
+        Genotype clone   = new Genotype(this.depth, this.arity, this.isTerm);
+        clone.expression = new String[expression.length];
+        for (int i = 0; i < expression.length; i++) {
+            clone.expression[i] = this.expression[i];
+        }
+        if (isTerm) {
+            clone.children = null;
+        } else {
+            clone.children = new Genotype[this.children.length];
+            for (int i = 0; i < children.length; i++)
+                clone.children[i] = this.children[i].clone();
+        }
+        return clone;
+    }
+
+    public Genotype getNodeAtDepth(int target) {
+        if (this.depth == target) {
+            return this;
+        } else {
+            ArrayList<Integer> genome = new ArrayList<Integer>();
+            for (int i = 0; i < this.arity; i++) {
+                if (children[i].getMaxNode() > target) {
+                    genome.add(i);
+            }
+            int targetBranch = genome.get(rand.nextInt(genome.size()));
+            return children[targetBranch].getNodeAtDepth(target);
+        }
+    }
+
+    public Genotype getSubTree(boolean term) {
+        if (term) {
+            if (arity == 0) {
+                return this.clone();
+            } else {
+                return children[rand.nextInt(arity)].getSubTree(true);
+            }
+        } else {
+            int target = (rand.nextInt(this.getMaxNode() - 1) + 1);
+            return this.getNodeAtDepth(target);
+        }
+    }
+}
---
+++ software/GeneticBot/additional/Phenotype.java
@@ -0,0 +1,275 @@
+/**
+ * A manifestation of the genotype (Genotype.java).
+ *
+ *
+ * @author:  Zak Edwards
+ * @version: 1.0 25/03/16
+ */
+
+import java.io.*;
+import java.util.Random;
+
+public class Phenotype {
+
+    /* --------------- Determine the behaviour of the agent in the Robocode environment ------------- */
+
+    /**
+     *  Declare global variables, characterising phenotypical behaviours.
+     */
+    final static
+        String PATH    = new String(".");
+        String PACKAGE = new String("");
+        String JARS    = new String("/opt/robocode/libs/*.jar");
+
+    final static int
+        chromosomes = 5,                 // Determine the number of possible (ph/g)enotypes
+        minDepth = Genotype.minDepth,    // Minimum depth of the Koza tree
+        maxDepth = Genotype.maxDepth;    // Maximum depth of the Koza tree
+
+    final static double
+        /* Crossover */
+        pCrossTerm = Genotype.pCrossTerm,                       // pCrossTerm = 0.10*0.87
+        pCrossRoot = Genotype.pCrossRoot,      // pCrossRoot = 0.30*0.87
+        pCrossFunc = Genotype.pCrossFunc,      // pCrossFunc = 0.60*0.87
+        pCrossJump = 0.05,
+        /* Mutation */
+        pMutateTerm = Genotype.pMutateTerm,    // pMutateTerm = 0.10*0.03
+        pMutateRoot = Genotype.pMutateRoot,    // pMutateRoot = 0.30*0.03
+        pMutateFunc = Genotype.pMutateFunc;    // pMutateFunc = 0.60*0.03
+
+    public static String
+        iteration = new String(),
+        phenes[]  = new String[chromosomes],
+        source    = new String(),
+        path      = "";
+
+    public int gen = 0, id = 0, numNodes = 0;
+    public double fitness = 0;
+
+    static Random rand = new Random(System.currentTimeMillis());
+    Genotype genes[]   = new Genotype[chromosomes];
+
+    public Phenotype(int gen, int id) {
+        this.gen = gen;
+        this.id  = id;
+        iteration = "GeneticBot-" + gen + "." + id;
+        path      = PACKAGE + "." + iteration;
+    }
+
+    /**
+     *  Recursive functions for Koza-type expression trees.
+     */
+    public int countNodes() {
+        this.numNodes = 0;
+        for (int i = 0; i < genes.length; i++)
+            numNodes += genes[i].countNodes();
+        return numNodes;
+    }
+
+    public void setDepths() {
+        for (Genotype expr : genes)
+            expr.setDepths(0);
+    }
+
+    /**
+     * Initialisation and construction methods.
+     */
+    public void init() {
+        for (int i = 0; i < chromosomes; i++) {
+            genes[i] = new Genotype(0);
+            genes[i].expand(0, 0);
+        }
+    }
+
+    public void construct() {
+        System.out.println(" Constructing expressions...");
+        for (int i = 0; i < chromosomes; i++) {
+            phenes[i] = genes[i].compose();
+            writeSource();
+        }
+    }
+
+    /**
+     * The groundwork of the agent, common to all iterations.
+     * To be written to a file for each respective iteration.
+     */
+    private void writeSource() {
+        source =
+            "package" + PACKAGE + ";"
+        + "\n"
+        + "\nimport robocode.*;"
+        + "\nimport robocode.util.Utils;"
+        + "\n"
+        + "\n"
+        + "\npublic class " + iteration + " extends Robot {"
+        + "\n"
+        + "\n    static double R1, R2;"
+        + "\n    R1 = R2 = 0;"
+        + "\n"
+        + "\n    /**"
+        + "\n     * run: The default behavior of GeneticBot"
+        + "\n     */"
+        + "\n    public void run() {"
+        + "\n        setAdjustGunForRobotTurn(true);"
+        + "\n        while(true) {"
+        + "\n            turnGunRight(Double.POSITIVE_INFINITY);"
+        + "\n            turnRight(R1);"
+        + "\n            setAhead(R2);"
+        + "\n        }"
+        + "\n    }"
+        + "\n"
+        + "\n    /**"
+        + "\n     * onScannedRobot: What to do when you see another robot"
+        + "\n     */"
+        + "\n    public void onScannedRobot(ScannedRobotEvent e) {"
+        + "\n        setAhead("          + phenes[0] + ");"
+        + "\n        setTurnRight("      + phenes[1] + ");"
+        + "\n        setTurnGunRight("   + phenes[2] + ");"
+        + "\n        setTurnRadarRight(" + phenes[3] + ");"
+        + "\n        setFire("           + phenes[4] + ");"
+        + "\n    }"
+        + "\n"
+        + "\n    /**"
+        + "\n     * onHitByBullet: What to do when you're hit by a bullet"
+        + "\n     */"
+        + "\n    public void onHitByBullet(HitByBulletEvent e) {"
+        + "\n    }"
+        + "\n"
+        + "\n    /**"
+        + "\n     * onHitWall: What to do when you hit a wall"
+        + "\n     */"
+        + "\n    public void onHitWall(HitWallEvent e) {"
+        + "\n    }"
+        + "\n"
+        + "\n}";
+    }
+
+    /**
+     * Crossover, mutation and replication methods.
+     */
+    public Phenotype crossover(Phenotype P, int gen, int id) {
+        Phenotype child = new Phenotype(gen, id);
+
+        int chromosome1 = rand.nextInt(chromosomes);
+        int chromosome2 = rand.nextInt(chromosomes);
+
+        for (int i = 0; i < chromosomes; i++) {
+            child.genes[i] = this.genes[i].clone();
+        }
+
+        while (chromosome1 == chromosome2) {
+            chromosome2 = rand.nextInt(chromosomes);
+        }
+
+        /* Perform a replacement operation on the chromosome */
+        if (rand.nextDouble() < pCrossRoot) {
+            if (rand.nextDouble() < pCrossJump) {
+                child.genes[chromosome1].replace(P.genes[chromosome2]);
+            } else {
+                child.genes[chromosome1].replace(P.genes[chromosome1]);   // replacement on equal chromosome indices
+            }
+        } else {
+            boolean term1 = (rand.nextDouble() < pCrossTerm) ? true : false;
+            boolean term2 = (rand.nextDouble() < pCrossTerm) ? true : false;
+            child.genes[chromosome1].insert(P.genes[chromosome1].getSubTree(term1));
+            child.genes[chromosome2].insert(P.genes[chromosome2].getSubTree(term2));
+        }
+
+        child.setDepths();
+        child.countNodes();
+
+        return child;
+    }
+
+    public Phenotype mutate(int gen, int id) {
+        Phenotype child = new Phenotype(gen, id);
+
+        int g = rand.nextInt(chromosomes);
+
+        if (rand.nextDouble() < pMutateRoot) {
+            child.genes[g] = new Genotype(0);
+            child.genes[g].expand(0, 0);
+        } else if (rand.nextDouble() < pMutateTerm) {
+            child.genes[g].mutateTerm();
+        } else {
+            child.genes[g].mutateFunc();
+        }
+
+        child.setDepths();
+        child.countNodes();
+
+        return child;
+    }
+
+    public Phenotype replicate(int gen, int id) {
+        Phenotype child = new Phenotype(gen, id);
+
+        for (int i = 0; i < chromosomes; i++) {
+            child.genes[i] = new Genotype(0);
+            child.genes[i].replace(this.genes[i]);
+        }
+
+        child.setDepths();
+        child.numNodes = this.numNodes;
+
+        return child;
+    }
+
+    /**
+     * File handling (iteration) methods.
+     */
+    public String compile() {
+        /**
+         * Write and compile the GeneticBot code for each iteration;
+         * return the absolute path to the resultant class file.
+         */
+        try {
+            FileWriter     fw = new FileWriter(PATH + "/" + iteration + ".java");
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(source);
+            bw.close();
+        } catch(Exception e) {
+            System.err.println("Err> " + e.getMessage());
+        }
+        try {
+            execute("javac -classpath " + JARS + " " + PATH + "/" + iteration + ".java");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return (PATH + "/" + iteration + ".class");
+    }
+
+    public static void clear(int gen, int size, int id) {
+        File oldJava, oldClass;
+        System.out.println("Commence removal of redundant iterations...");
+        for (int i = 0; i < size; i++) {
+            if ((i == id || gen == 0) && i < 10) {
+                continue;
+            }
+            oldJava  = new File(PATH + "/" + "GeneticBot-" + iteration + "." + i + ".java");
+            oldClass = new File(PATH + "/" + "GeneticBot-" + iteration + "." + i + ".class");
+            oldJava.delete();
+            System.out.println("  Removed " + oldJava + ".");
+            oldClass.delete();
+            System.out.println("  Removed " + oldClass + ".");
+        }
+    }
+
+    public static void execute(String cmd) throws Exception {
+        Process p = Runtime.getRuntime().exec(cmd);
+        printMsg(cmd + "stdout> ", p.getInputStream());
+        printMsg(cmd + "stderr> ", p.getErrorStream());
+        p.waitFor();
+        if (p.exitValue() != 0) {
+            System.out.println(cmd + " exited with value " + p.exitValue());
+        }
+    }
+
+    private static void printMsg(String str, InputStream ins) throws Exception {
+        String line = null;
+        BufferedReader in = new BufferedReader(new InputStreamReader(ins));
+        while ((line = in.readLine()) != null) {
+            System.out.println(str + " " + line);
+        }
+    }
+}
